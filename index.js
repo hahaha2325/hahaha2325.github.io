e9571d227af76962879fe41fd59dc2d6cc9127ee
hahaha2325 <tantun0621@gmail.com>
2025-05-28 09:31:16 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var myInput = document.getElementById("myInput")
+    var output = "こんにちは！" + myInput.value + "さん。これから日本について勉強していきましょう！"
+    document.getElementById("myOutput").innerHTML = output
+}
